fbfdebf4bb8a9c22a8267308645ddbf0d915bdda
Jose_Rivera <joseriveradl@gmail.com>
2023-01-07 00:02:10 -0600

    :construction: working with localStorage

--- src/App/index.js
+++ src/App/index.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import { AppUI } from './AppUI';
 
-const defaultTodos = [
+/* const defaultTodos = [
   { text: 'Cortar cebolla', completed: true },
   { text: 'Tomar el cursso de intro a React', completed: false },
   { text: 'Llorar con la llorona', completed: false },
   { text: 'LALALALAA', completed: false },
-];
+]; */
 
 
 
 function App() {
 
-  const [todos, setTodos] = React.useState(defaultTodos)
+  const localStorageTodos = localStorage.getItem('TODOS_V1')
+
+  let parsedTodos
+
+  if (!localStorageTodos) {
+    localStorage.setItem('TODOS_V1', JSON.stringify([]))
+    parsedTodos=[]
+  } else {
+    parsedTodos = JSON.parse(localStorageTodos)
+  }
+
+  const [todos, setTodos] = React.useState(parsedTodos)
   const [searchValue, setSearchValue] = React.useState('');
 
   const completedTodos = todos.filter(todo => !!todo.completed).length
@@ -30,13 +41,19 @@ function App() {
     })
   }
 
+  const saveTodos = (newTodos) => {
+    const stringifiedTodos = JSON.stringify(newTodos)
+    localStorage.setItem('TODOS_V1', stringifiedTodos)
+    setTodos(newTodos)
+  }
+
   const completeTodo = (text) => {
     const todoIndex = todos.findIndex(todo => todo.text === text)
 
     const newTodos= [...todos]
     newTodos[todoIndex].completed = true
 
-    setTodos(newTodos)
+    saveTodos(newTodos)
     /* todos[todoIndex] = {
       text: todos[todoIndex].text,
       completed: true,
@@ -50,7 +67,7 @@ function App() {
     //sacar elementos indicar desde que posicion hasta que posicion
     newTodos.splice(todoIndex, 1)
 
-    setTodos(newTodos)
+    saveTodos(newTodos)
 
   }
 
